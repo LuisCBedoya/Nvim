9240057003f4c483d4af10e8202dff4f165de011
luiscbedoya <bedoyaandocillaluiscarlos@gmail.com>
2023-10-24 18:06:37 -0500

    fix keymaps

--- lua/keybindings/init.lua
+++ lua/keybindings/init.lua
@@ -1,43 +1,46 @@
-vim.g.mapleader = " "
+vim.g.mapleader = ' '
 local map = vim.api.nvim_set_keymap
 
 -- bufferline next prev
-map("n", "<tab>", ":bnext<CR>", { noremap = true, silent = false })
-map("n", "<s-tab>", ":bprevious<CR>", { noremap = true, silent = false })
+map('n', '<tab>', ':bnext<CR>', { noremap = true, silent = false })
+map('n', '<s-tab>', ':bprevious<CR>', { noremap = true, silent = false })
 --
 -- tree
-map("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
+map('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
 
-map("v", "<", "<gv", { noremap = true, silent = true })
-map("v", ">", ">gv", { noremap = true, silent = true })
+map('v', '<', '<gv', { noremap = true, silent = true })
+map('v', '>', '>gv', { noremap = true, silent = true })
 
-map("i", "jk", "<ESC>", { noremap = true, silent = false })
-map("i", "jk", "<ESC>", { noremap = true, silent = false })
+map('n', '<C-u>', '{', { noremap = true, silent = true })
+map('n', '<C-d>', '}', { noremap = true, silent = true })
 
-map("v", "jk", "<ESC>", { noremap = true, silent = false })
-map("v", "jk", "<ESC>", { noremap = true, silent = false })
+map('i', 'jk', '<ESC>', { noremap = true, silent = false })
+map('i', 'jk', '<ESC>', { noremap = true, silent = false })
 
-map("n", "v", "V", { noremap = true, silent = false })
-map("n", "v", "V", { noremap = true, silent = false })
+map('v', 'jk', '<ESC>', { noremap = true, silent = false })
+map('v', 'jk', '<ESC>', { noremap = true, silent = false })
 
-map("n", "<leader>ñ", ">>", { noremap = true, silent = false })
-map("n", "ñ", "<<", { noremap = true, silent = false })
+map('n', 'v', 'V', { noremap = true, silent = false })
+map('n', 'v', 'V', { noremap = true, silent = false })
 
-map("v", "<leader>ñ", ">>", { noremap = true, silent = false })
-map("v", "ñ", "<<", { noremap = true, silent = false })
+map('n', '<leader>ñ', '>>', { noremap = true, silent = false })
+map('n', 'ñ', '<<', { noremap = true, silent = false })
 
-map("n", "t", "%", { noremap = true, silent = false })
-map("v", "t", "%", { noremap = true, silent = false })
+map('v', '<leader>ñ', '>>', { noremap = true, silent = false })
+map('v', 'ñ', '<<', { noremap = true, silent = false })
 
-map("n", "m", "#", { noremap = true, silent = false })
-map("v", "m", "#", { noremap = true, silent = false })
+map('n', 't', '%', { noremap = true, silent = false })
+map('v', 't', '%', { noremap = true, silent = false })
 
-map("n", "<C-Space>", ":", { noremap = true, silent = false })
+map('n', 'm', '#', { noremap = true, silent = false })
+map('v', 'm', '#', { noremap = true, silent = false })
 
-map(
-    "n",
-    "<C-u>",
-    "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<cr>",
-    { noremap = true, silent = true }
-)
-map("n", "<C-d>", "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<cr>", { noremap = true, silent = true })
+map('n', '<C-Space>', ':', { noremap = true, silent = false })
+
+-- map(
+--   'n',
+--   '<C-u>',
+--   "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<cr>",
+--   { noremap = true, silent = true }
+-- )
+-- map('n', '<C-d>', "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<cr>", { noremap = true, silent = true })
