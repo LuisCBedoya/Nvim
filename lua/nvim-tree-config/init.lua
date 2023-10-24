7db33b9d254f4571756f308dd342d1676ca7be8f
luiscbedoya <bedoyaandocillaluiscarlos@gmail.com>
2023-10-24 15:15:05 -0500

    null-ls and nvim-tree files

--- lua/nvim-tree-config/init.lua
+++ lua/nvim-tree-config/init.lua
@@ -1,16 +1,16 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
-require("nvim-tree").setup({
-    sort_by = "case_sensitive",
-    view = {
-        width = 50,
-        side = "right",
-    },
-    renderer = {
-        group_empty = true,
-    },
-    filters = {
-        dotfiles = true,
-    },
+require('nvim-tree').setup({
+  sort_by = 'case_sensitive',
+  view = {
+    width = 40,
+    side = 'left',
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
 })
